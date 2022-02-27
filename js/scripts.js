1a93ff923ca408095aaa470fb18e1e4166718be9
Jonathan Cheng <jcheng461@gmail.com>
2022-02-27 22:28:09 +0900

    add separate forms for subtraction, multiplication, and division. Now user can use calculator for each of these, albiet by typing in different boxes, and clicking on different buttons

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,40 @@ function divide(n1, n2) {
 
 // User interface logic:
 $(document).ready(function () {
+  // Addition
   $("form#add").submit(function (event) {
     event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    // Prints out the result
+    $("#additionOutput").text(result);
+  });
+  // Subtraction
+  $("form#sub").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    // Prints out the result
+    $("#subtractionOutput").text(result);
+  });
+  // Multiplication
+  $("form#mult").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mult1").val());
+    const number2 = parseInt($("#mult2").val());
+    const result = multiply(number1, number2);
+    // Prints out the result
+    $("#multiplicationOutput").text(result);
+  });
+  // Division
+  $("form#div").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    // Prints out the result
+    $("#divisionOutput").text(result);
   });
 });
