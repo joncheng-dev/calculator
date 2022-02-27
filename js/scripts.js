222eb5f3de2a5249ef17636395bddad6e5b6237e
Jonathan Cheng <jcheng461@gmail.com>
2022-02-27 18:55:35 +0900

    add user interface to scripts. Now user can enter in a form for numbers to be added. Removes alert to tell user of answer. Answer is now displayed on screen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
+// Business logic:
 function add(n1, n2) {
-  return n1 + n2
+  return n1 + n2;
 }
 
 function subtract(n1, n2) {
-  return n1 - n2
+  return n1 - n2;
 }
 
 function multiply(n1, n2) {
-  return n1 * n2
+  return n1 * n2;
 }
 
 function divide(n1, n2) {
-  return n1 / n2
+  return n1 / n2;
 }
 
-const number1 = parseInt(prompt("Enter a number: "))
-const number2 = parseInt(prompt("Enter another: "))
-const result = add(number1, number2)
-
-alert("The sum is: " + result)
+// User interface logic:
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
